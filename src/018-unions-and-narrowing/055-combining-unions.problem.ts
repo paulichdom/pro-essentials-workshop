4ac7bf7fd0be9a53f1f617131dffba7c94f2e869
Domagoj <paulic.domagoj@gmail.com>
2024-08-18 13:50:32 +0200

    solved combining unions 055

--- src/018-unions-and-narrowing/055-combining-unions.problem.ts
+++ src/018-unions-and-narrowing/055-combining-unions.problem.ts
@@ -1,23 +1,26 @@
-import { Equal, Expect } from "@total-typescript/helpers";
+import { Equal, Expect } from '@total-typescript/helpers';
 
-type HttpCode = "400" | "401" | "404" | "500" | "200" | "201" | "204";
+type HttpSuccessCode = '200' | '201' | '204';
+type HttpErrorCode = '400' | '401' | '404' | '500';
 
-const handleErrorCase = (code: string) => {
+type HttpCode = HttpErrorCode | HttpSuccessCode;
+
+const handleErrorCase = (code: HttpErrorCode) => {
   // An imaginary function where we only handle the errors
 
-  type test = Expect<Equal<typeof code, "400" | "401" | "404" | "500">>;
+  type test = Expect<Equal<typeof code, '400' | '401' | '404' | '500'>>;
 };
 
-const handleSuccessCase = (code: string) => {
+const handleSuccessCase = (code: HttpSuccessCode) => {
   // An imaginary function where we only handle the success cases
 
-  type test = Expect<Equal<typeof code, "200" | "201" | "204">>;
+  type test = Expect<Equal<typeof code, '200' | '201' | '204'>>;
 };
 
 const handleAllCase = (code: HttpCode) => {
   // An imaginary function where we handle all the cases
 
   type test = Expect<
-    Equal<typeof code, "200" | "201" | "204" | "400" | "401" | "404" | "500">
+    Equal<typeof code, '200' | '201' | '204' | '400' | '401' | '404' | '500'>
   >;
 };
