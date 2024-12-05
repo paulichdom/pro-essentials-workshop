8901efed8482bcf751cb8e70e90131d63b94c698
Domagoj <paulic.domagoj@gmail.com>
2024-12-05 09:38:06 +0100

    solve extending other classes 114

--- src/030-classes/114-extending-other-classes.problem.ts
+++ src/030-classes/114-extending-other-classes.problem.ts
@@ -2,15 +2,13 @@ import { expect, it } from "vitest";
 
 type ViewMode = "hidden" | "visible" | "selected";
 
-class CanvasNode {
+class Shape {
   #x: number;
   #y: number;
-  #viewMode: ViewMode;
 
-  constructor(options?: { x: number; y: number; viewMode?: ViewMode }) {
+  constructor(options?: { x: number; y: number }) {
     this.#x = options?.x ?? 0;
     this.#y = options?.y ?? 0;
-    this.#viewMode = options?.viewMode ?? "visible";
   }
 
   get position() {
@@ -24,6 +22,16 @@ class CanvasNode {
     this.#x = x;
     this.#y = y;
   }
+}
+
+class CanvasNode extends Shape {
+  #viewMode: ViewMode;
+
+  constructor(options?: { x: number; y: number; viewMode?: ViewMode }) {
+    super(options);
+    this.#viewMode = options?.viewMode ?? "visible";
+  }
+
 
   hide() {
     this.#viewMode = "hidden";
