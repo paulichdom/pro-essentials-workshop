565f853f9ee6fa759ae0254dbae74fe378f860ba
paulichdom <paulic.domagoj@gmail.com>
2026-01-28 08:22:37 +0100

    refactor: restructure myFetch call to use inline object for options

--- src/050-the-weird-parts/152-excess-properties-warnings.problem.ts
+++ src/050-the-weird-parts/152-excess-properties-warnings.problem.ts
@@ -5,18 +5,16 @@ interface FetchOptions {
   body?: string;
 }
 
-const options = {
-  url: "/",
-  method: "GET",
-  headers: {
-    "Content-Type": "application/json",
-  },
-  // @ts-expect-error
-  search: new URLSearchParams({
-    limit: "10",
-  }),
-};
-
 const myFetch = async (options: FetchOptions) => {};
 
-myFetch(options);
+myFetch({
+    url: "/",
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    // @ts-expect-error
+    search: new URLSearchParams({
+      limit: "10",
+    }),
+  });
